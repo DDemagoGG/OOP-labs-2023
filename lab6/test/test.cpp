683dda43019254ec66444a90bbc1bf6cfaf162e0
DDemagoGG <lyadiez@mail.ru>
2023-11-28 03:11:30 +0300

    done lab6 without tests

---
+++ lab6/test/test.cpp
@@ -0,0 +1,9 @@
+#include "bull.hpp"
+#include "dragon.hpp"
+#include "frog.hpp"
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
